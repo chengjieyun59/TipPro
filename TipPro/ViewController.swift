e1fd8e0715de8b3a6263e3dc6d0560b17d9afa7e
chengjieyun59 <chengjieyun59@gmail.com>
2017-07-11 16:58:16 -0700

    TipPro calculates amount of tip
    The user can input the amount of money on their bill, and choose 15%, 18%, or 20% tip amount. The iOS app would then display the amount of tip and the total amount.

--- TipPro/ViewController.swift
+++ TipPro/ViewController.swift
@@ -6,20 +6,75 @@
 //  Copyright © 2017 Jie-Yun Cheng. All rights reserved.
 //
 
+// There is no README file
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    @IBOutlet weak var tipAmountField: UITextField!
+    @IBOutlet weak var totalAmountField: UITextField!
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        if let billAmount = Double(billAmountField.text!) {
+            var tipPercentage = 0.0
+            
+            switch tipSelector.selectedSegmentIndex {
+            case 0:
+                tipPercentage = 0.15
+            case 1:
+                tipPercentage = 0.18
+            case 2:
+                tipPercentage = 0.20
+            default:
+                break
+            }
+            
+            let roundedBillAmount = round(100 * billAmount) / 100
+            let tipAmount = roundedBillAmount * tipPercentage
+            let roundedTipAmount = round(100*tipAmount)/100
+            let totalAmount = roundedBillAmount + roundedTipAmount
+            
+            if (!billAmountField.isEditing) {
+                billAmountField.text = String(format: "%.2f", roundedBillAmount)
+            }
+            tipAmountField.text = String(format: "%.2f", roundedTipAmount)
+            totalAmountField.text = String(format: "%.2f", totalAmount)
+        } else {
+            //show error
+            billAmountField.text = ""
+            tipAmountField.text = ""
+            totalAmountField.text = ""
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
 }
 
